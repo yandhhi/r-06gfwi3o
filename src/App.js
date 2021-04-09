247c242d61874f2d52e3a5eeea170f16d9861b1c
Andres Higuita <yecid3257@live.com>
2021-04-08 21:51:10 -0500

    add component Welcome

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import Welcome from './Welcome';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      names: ["Juan", "Pedro", "Germán"]
-    }
+      names: ['Juan', 'Pedro', 'Germán'],
+    };
   }
 
   render() {
     return (
       <div>
-        {this.state.names.map((name) =>
+        {this.state.names.map((name) => (
           <Welcome name={name} />
-        )}
+        ))}
       </div>
     );
   }
